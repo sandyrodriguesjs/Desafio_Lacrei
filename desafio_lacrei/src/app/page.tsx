988c9ffaa52b0642e640203a2a2ce29995e0feef
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-12-08 15:38:23 -0300

    first commit

---
+++ desafio_lacrei/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Home from "./pages/1l/Inicio/page";
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+export default App;
---
+++ desafio_lacrei/src/app/pages/1l/Inicio/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from "@/app/components/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>HomePage</p>
+        <Button>Click Me</Button>
+    </div>
+  );
+}
